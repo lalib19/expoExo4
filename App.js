d6cba9e5a20a7af3b80f7fdcb46172c32be8e6b9
louis alibert <louis.alib19@gmail.com>
2022-05-10 14:21:29 +0200

    expo exercise 4

--- App.js
+++ App.js
@@ -4,8 +4,26 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      
+      <View style={styles.bigContainer1}>
+
+        <View style={styles.midContainer1}>
+          <View style={styles.container1}></View>
+          <View style={styles.container2}></View>
+          <View style={styles.container3}></View>
+        </View>
+
+        <View style={styles.midContainer2}>
+          <View style={styles.container4}></View>
+          <View style={styles.container5}></View>
+        </View>
+
+      </View> 
+
+      <View style={styles.bigContainer2}>
+      </View> 
+
     </View>
   );
 }
@@ -14,7 +32,43 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    
   },
-});
+  bigContainer1: {
+    flexDirection: 'row',
+    flex:1,
+  },
+  bigContainer2: {
+    flex: 1,
+    backgroundColor: "grey"
+  },
+  midContainer1: {
+    flex: 1,
+    flexDirection: 'column',
+  },
+  midContainer2: {
+    flex: 1,
+    flexDirection: 'row'
+  },
+  container1: {
+    flex: 5,
+    backgroundColor: "purple",
+  },
+  container2: {
+    flex: 2,
+    backgroundColor: "yellow",
+  },
+  container3: {
+    flex: 2,
+    backgroundColor: "pink",
+  },
+  container4 : {
+    flex: 3,
+    backgroundColor: "red",
+  },
+  container5: {
+    flex: 1,
+    backgroundColor: "blue"
+  }
+  }
+);
